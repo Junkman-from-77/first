159be89e9b0c16157723ab67562988188b1ddf57
Mad-KoT3 <130034650+Mad-KoT3@users.noreply.github.com>
2024-01-31 13:39:01 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <meta type="description" content="">
+    <meta type="keywords" content="">
+    <title>Mad_KoT3 - Главная</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="wrapper">
+    <header>
+        <h1>TEST_PROJ</h1>
+    </header>
+    <section>
+        <div class="projects">
+            <div class="proj">
+                <a href="not-archive/index.php">Нотный архив</a>
+            </div>
+            <div class="proj">
+                <a href="metro/2006/index.php">Транспортный раздел</a>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p>Designed by "Dva pritopa - tri prikhlopa prod".</p>
+        <p>Copyright © 2024 Mad_KoT3</p>
+    </footer>
+    </div>
+</body>
+</html>
