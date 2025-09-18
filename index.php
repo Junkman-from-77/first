bb572fa794bf1383fac495337700b5b95a8cfbfb
Junkman <130034650+Junkman-from-77@users.noreply.github.com>
2025-09-18 12:05:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <meta type="description" content="">
+    <meta type="keywords" content="">
+    <title>Junkman - Главная</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="wrapper">
+    <header>
+        <h1>ПРОБА ПЕРА</h1>
+	<p>Исходный код: <a  href="https://github.com/Mad-KoT3/first/tree/main" target="_blank">https://github.com/Mad-KoT3/first/tree/main</a></p>
+	<p>В общем, захотел я посмотреть, что это за зверь такой - бэкенд. И пошёл по старой доброй дорожке - изучать PHP, MySQL и запиливать что-то своё. Текущим результатом не удовлетворён, осуществил далеко не всё из задуманного.<br>
+	Данный пет-проект будет написан заново.</p>
+	<p>Зачем нужны такие перемены? Параллельно с разработкой этого сайта я почитал, какие вообще бывают архитектурные разновидности сайтов. Сделал вывод, что небольшие сайты в настоящее время актуально создавать на fullstack-фреймворке Next. Для крупных проектов следует писать отдельный бэкенд на компилируемых языках (Java, Go), чтобы была возможность "переварить" большое количество запросов.</p>
+	<p>В мой сайт вошли три раздела, связанные с музыкой, ретро техникой, первыми опытами с растровой графикой.
+    </header>
+    <section>
+        <div class="projects">
+            <div class="proj">
+                <a href="not-archive/index.php">Нотный архив</a>
+            </div>
+            <div class="proj">
+                <a href="metro/2006/index.php">Транспортный раздел</a>
+            </div>
+            <div class="proj">
+                <a href="ps/index.html">Работы в Фотошопе/GIMP</a>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p>Designed by "Dva pritopa - tri prikhlopa prod".</p>
+        <p>Copyright © Junkman</p>
+    </footer>
+    </div>
+</body>
+</html>
